748a602cdee173fb28ad9d61940d674d805d3790
jvcanavarro <jvcanavarro@gmail.com>
2019-05-31 17:29:36 -0300

    travis teste

---
+++ principal.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def soma(x, y):
+	return(x + y)
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import pytest
+from principal import soma
+
+def test_soma():
+	assert soma(2,4) == 6
+
+
